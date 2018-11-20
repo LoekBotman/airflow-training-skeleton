cddc8f249f55cbe5e27f2e1e637e1a677093e982
Loek Botman <lbotman@bol.com>
2018-11-20 11:39:47 +0100

    Added hello date python DAG

--- dags/dag_python.py
+++ dags/dag_python.py
@@ -2,8 +2,10 @@ import airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-def print_exec_date(excecution_date, **context):
+
+def print_exec_date(execution_date):
     print(execution_date)
+
 
 dag = DAG(
     dag_id="hello_airflow",
@@ -15,7 +17,7 @@ dag = DAG(
 
 PythonOperator(
     task_id="print_exec_date",
-    python_calable=print_exec_date,
+    python_calable=print_exec_date(),
     provide_context=True,
     dag=dag
 )
