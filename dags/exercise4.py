1c626ca67275630f7130806b7261868f1a46aa63
Loek Botman <lbotman@bol.com>
2018-11-22 11:16:24 +0100

    Exercise 4

--- dags/exercise4.py
+++ dags/exercise4.py
@@ -1,6 +1,10 @@
 import airflow
 from airflow import DAG
 from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
+from airflow.contrib.operators.dataproc_operator import(
+    DataprocClusterCreateOperator,
+    DataProcPySparkOperator,
+    DataprocClusterDeleteOperator)
 
 
 args = {"owner": "Loek",
@@ -14,13 +18,28 @@ dag = DAG(
 
 pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
     task_id="pgsl_to_gcs",
-    sql = """SELECT * FROM 
+    sql ="""SELECT * FROM 
     land_registry_price_paid_uk WHERE 
     transfer_date = '{{ ds }}'""",
-    bucket = "airflow-training-data-loek/pgsl_to_gcs",
-    filename = "daily_load_{{ ds }}.json",
+    bucket="airflow-training-data-loek",
+    filename="pgsl_to_gcs/daily_load_{{ ds }}.json",
     postgres_conn_id="postgres_table",
     dag=dag
 )
+
+# dataproc_create_cluster = DataprocClusterCreateOperator(
+# #     cluster_name="analyse-pricing-{{ ds }}",
+# #     project_id="Training Boldotcom - lbotman",
+# #     num_workers=2,
+# #     zone="europe-west4-a",)
+# #
+# # compute_aggregates = DataProcPySparkOperator(
+# #     main="../other/build_statistics.py",
+# #     cluster_name="analyse-pricing-{{ ds }}",
+# #     arguments=["{{ ds }}"],)
+# #
+# #
+# # dataproc_delete_cluster = DataprocClusterDeleteOperator(
+# #     cluster_name="analyse-pricing-{{ ds }}")
 
 pgsl_to_gcs
