2a9f5b88b18bc291181a661b832da473a300b34f
Loek Botman <lbotman@bol.com>
2018-11-22 13:28:22 +0100

    Exercise 4

--- dags/exercise4.py
+++ dags/exercise4.py
@@ -6,10 +6,16 @@ from airflow.contrib.operators.dataproc_operator import(
     DataprocClusterCreateOperator,
     DataProcPySparkOperator,
     DataprocClusterDeleteOperator)
-
+from other.http_to_gcs import HttpToGcsOperator
 
 args = {"owner": "Loek",
         "start_date": airflow.utils.dates.days_ago(3)}
+
+project_id = "611e4210f1606867"
+
+http_end_point = "https://europe-west1-gdd-airflow-training.cloudfunctions.net/airflow-training-transform-valutas?date={{ ds }}&from=GBP&to=EUR"
+
+bucket = "airflow-training-data-loek"
 
 dag = DAG(
     dag_id="exercise4",
@@ -22,27 +28,40 @@ pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
     sql ="""SELECT * FROM 
     land_registry_price_paid_uk WHERE 
     transfer_date = '{{ ds }}'""",
-    bucket="airflow-training-data-loek",
+    bucket=bucket,
     filename="land_registry_price_paid_uk/daily_load_{{ ds }}.json",
     postgres_conn_id="postgres_table",
     dag=dag
 )
 
-# dataproc_create_cluster = DataprocClusterCreateOperator(
-#     cluster_name="analyse-pricing-{{ ds }}",
-#     project_id="airflowbolcom-611e4210f1606867",
-#     num_workers=2,
-#     zone="europe-west4-a",)
-#
-# compute_aggregates = DataProcPySparkOperator(
-#     main="../other/build_statistics.py",
-#     cluster_name="analyse-pricing-{{ ds }}",
-#     arguments=["{{ ds }}"],)
-#
-#
-# dataproc_delete_cluster = DataprocClusterDeleteOperator(
-#     cluster_name="analyse-pricing-{{ ds }}",
-#     trigger_rule=TriggerRule.ONE_SUCCESS)
-#
-# [pgsl_to_gcs, dataproc_create_cluster] >> compute_aggregates >> dataproc_delete_cluster
-pgsl_to_gcs
+dataproc_create_cluster = DataprocClusterCreateOperator(
+    dag=dag,
+    cluster_name="analyse-pricing-{{ ds }}",
+    project_id=project_id,
+    num_workers=2,
+    zone="europe-west4-a"
+)
+
+compute_aggregates = DataProcPySparkOperator(
+    dag=dag,
+    main="../other/build_statistics.py",
+    cluster_name="analyse-pricing-{{ ds }}",
+    arguments=["{{ ds }}"]
+)
+
+dataproc_delete_cluster = DataprocClusterDeleteOperator(
+    dag=dag,
+    cluster_name="analyse-pricing-{{ ds }}",
+    project_id=project_id,
+    trigger_rule=TriggerRule.ALL_DONE
+)
+
+http_to_gcs = HttpToGcsOperator(
+    dag=dag,
+    endpoint=http_end_point,
+    gcs_bucket=bucket,
+    gcs_path="exchange_rates/"
+)
+
+
+[pgsl_to_gcs, dataproc_create_cluster, http_to_gcs] >> compute_aggregates >> dataproc_delete_cluster
---
+++ dags/exercise_only_http.py
@@ -0,0 +1,24 @@
+import airflow
+from other.http_to_gcs import HttpToGcsOperator
+
+args = {"owner": "Loek",
+        "start_date": airflow.utils.dates.days_ago(3)}
+
+http_end_point = "https://europe-west1-gdd-airflow-training.cloudfunctions.net/airflow-training-transform-valutas?date={{ ds }}&from=GBP&to=EUR"
+
+bucket = "airflow-training-data-loek"
+
+dag = DAG(
+    dag_id="exercise_only_http",
+    default_args=args,
+    schedule_interval="0 0 * * *"
+)
+
+http_to_gcs = HttpToGcsOperator(
+    dag=dag,
+    endpoint=http_end_point,
+    gcs_bucket=bucket,
+    gcs_path="exchange_rates/"
+)
+
+http_to_gcs
